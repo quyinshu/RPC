4edaa89e345c2120e1d00419504727694a7cf150
qys <1098005953@qq.com>
2025-04-15 05:30:27 +0800

    modify RPC

--- qys.rpc/src/main/java/bean/NettyInitial.java
+++ qys.rpc/src/main/java/bean/NettyInitial.java
@@ -12,7 +12,6 @@ import constants.Constans;
 import factory.ZooKeeperFactory;
 import handler.ServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -26,13 +25,11 @@ import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * `NettyInitial` 类实现了 `ApplicationListener` 接口，用于监听 `ContextRefreshedEvent` 事件，以便在应用程序启动时引导并初始化一个 Netty 服务器。该类负责设置必要的 Netty 配置，如事件循环、服务器管道和处理器。它还会将服务器实例注册到 ZooKeeper 服务中，以实现服务发现。
- *
  * 主要功能包括：
  * - 使用 `ServerBootstrap` 配置并启动 Netty 服务器。
  * - 为服务器端管道初始化处理器，用于执行特定任务，如解码、编码和业务逻辑处理。
  * - 将服务器实例信息注册到 ZooKeeper 节点。
  * - 在发生异常时，优雅地关闭服务器及相关资源。
- *
  * 实现接口：
  * - `ApplicationListener<ContextRefreshedEvent>`：确保服务器作为 Spring 应用程序生命周期的一部分，在应用程序上下文刷新时启动。
  */
